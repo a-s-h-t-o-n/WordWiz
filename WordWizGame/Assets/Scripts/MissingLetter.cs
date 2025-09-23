83fbd36f4f3d1da0ca341a432603d7e817927237
a-s-h-t-o-n <ashton.king@student.uts.edu.au>
2025-09-23 17:48:59 +1000

    rough version of missing letter game complete

--- WordWizGame/Assets/Scripts/MissingLetter.cs
+++ WordWizGame/Assets/Scripts/MissingLetter.cs
@@ -7,7 +7,9 @@ public class MissingLetter : MonoBehaviour
     public bool isDebug;
     private string word;
     private char letter;
-    private List<char> letters = new List<char>();
+    private List<char> letters = new List<char>{'0', '0', '0'};
+    private int numOptions = 3;
+    private TextMeshProUGUI selectedAnswer;
 
     // replace this w/ SO or similar data object
     private string[] words = { "egg", "apple", "shoe", "bee", "flower", "tree", "rock", "juice", "colour", "fruit", "meat", "food" };
@@ -18,8 +20,7 @@ public class MissingLetter : MonoBehaviour
     {
         GenerateRandomWord();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
@@ -42,9 +43,7 @@ public class MissingLetter : MonoBehaviour
 
     public void GenerateWrongLetters()
     {
-        letters.Insert(0, word[0]); 
-        letters.Insert(1, word[0]);
-        
+        letters[0] = letters[1] = word[0];
         while (word.Contains(letters[0]))
         {
             letters[0] = (char)(122 - Random.Range(0, 25));
@@ -56,7 +55,7 @@ public class MissingLetter : MonoBehaviour
         }
 
         // add correct letter to letter option list
-        letters.Insert(2, letter);
+        letters[2] = letter;
         
         // Technically we should randomise, but sorting will achieve basically the same thing here. Alphabetical order won't give any indication as to which is correct.
         letters.Sort();
@@ -64,20 +63,37 @@ public class MissingLetter : MonoBehaviour
         SetUIElements();
     }
 
-    public void SetUIElements()
+    private void SetUIElements()
     {
         buttonText1.text = letters[0].ToString();
         buttonText2.text = letters[1].ToString();
         buttonText3.text = letters[2].ToString();
-        wordText1.text = word.Replace(letter, '_');
+        wordText1.text = word.Substring(0, word.IndexOf(letter)) + "_" + word.Substring(word.IndexOf(letter) + 1);
         
-        if (isDebug) DebugDebugItAll();
+        if (isDebug) DedededeDebugItAll();
     }
 
-    private void DebugDebugItAll()
+    private void DedededeDebugItAll()
     {
         Debug.Log($"the word is {word}");
         Debug.Log($"the missing letter is {letter}");
         Debug.Log($"the letter options are {letters[0]} & {letters[1]} & {letters[2]}");
+    }
+
+    public void SetSelection(TextMeshProUGUI selection)
+    {
+        selectedAnswer = selection;
+    }
+
+    public void CheckSelection()
+    {
+        if (selectedAnswer != null && selectedAnswer.text == letter.ToString())
+        {
+            Debug.Log("RIGHT ANSWER.");
+        }
+        else
+        {
+            Debug.Log("WRONG ANSWER.");
+        }
     }
 }
