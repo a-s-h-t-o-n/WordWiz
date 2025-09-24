51af965616fc83c78a55762aeefe73a5f1ebdb09
a-s-h-t-o-n <ashton.king@student.uts.edu.au>
2025-09-24 21:15:13 +1000

    formatting and all that jazz

---
+++ WordWizGame/Assets/Scripts/ImageMatch.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageMatch : MonoBehaviour
+{
+    [SerializeField] private List<Image> imageList;
+    [SerializeField] private List<Image> imageObjects;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
