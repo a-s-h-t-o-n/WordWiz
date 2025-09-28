55b35f69332cb9d43f77c326351194571e82f4fa
cassandras12 <cassandra.shearwood@student.uts.edu.au>
2025-09-29 01:25:37 +1000

    erase old sentence and spawn new one on clicking next/done button

--- WordWizGame/Assets/Cass/CutUpSentences.cs
+++ WordWizGame/Assets/Cass/CutUpSentences.cs
@@ -9,27 +9,46 @@ public class CutUpSentences : MonoBehaviour
 {
     public TextAsset sentencesFile;
     public string[] lines;
-    
+
     public int NumOfSentences = 3;
 
     public RectTransform wordArea;
-    
+
     //PREFAB
     public GameObject wordBox;
 
     public GameObject sentencePlacementArea;
+    GridLayoutGroup grid;
+    public string fullSentence;
+    public string[] words;
+
+    bool correct = true;
+    public GameObject answerImage;
+    public TextMeshProUGUI answerText;
+    public Button nextButton;
+
+    public TextMeshProUGUI textCheck;
+    public Transform wordCheck;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         lines = SplitLines(sentencesFile.text);
+        grid = sentencePlacementArea.GetComponent<GridLayoutGroup>();
+        SpawnNewSentence();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        TestCurrentSentence();
+        //CheckCurrentSentence();
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateCutUpSentence();
+        }*/
     }
 
     string[] SplitLines(string text)
@@ -46,17 +65,17 @@ public class CutUpSentences : MonoBehaviour
     public void GenerateCutUpSentence()
     {
         //destroy old text boxes
-        foreach(Transform child in wordArea)
+        foreach (Transform child in wordArea)
         {
             Destroy(child.gameObject);
         }
 
         //get random sentence in file
         int randomSentenceNum = Random.Range(0, lines.Length);
-        string fullSentence = lines[randomSentenceNum];
-        
+        fullSentence = lines[randomSentenceNum];
+
         //get random sentence and split into words
-        string[] words = fullSentence.Split(' ');
+        words = fullSentence.Split(' ');
 
         //for each word generate a word box
         foreach (string word in words)
@@ -66,17 +85,66 @@ public class CutUpSentences : MonoBehaviour
             text.text = word;
         }
 
-
         //}
     }
 
 
-    void TestCurrentSentence()
+    public bool CheckCurrentSentence()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //string checkSentence = "";
+        answerText.text = "RIGHT";
+
+
+        //get word in grid layout of sentence placement
+        //check for all word in sentence that grid layout words == sentence words
+
+        //for all in the grid
+
+        for (int i = 0; i < words.Length; i++)
         {
-            GenerateCutUpSentence();
+            wordCheck = grid.transform.GetChild(i);
+
+            if(wordCheck.childCount > 0)
+        {
+            textCheck = wordCheck.GetChild(0).GetComponentInChildren<TextMeshProUGUI>();
+            if (textCheck == null || wordCheck == null)
+            {
+                answerText.text = "WRONG";
+                correct = false;
+                break;
+            }
+
+            if (textCheck.text != words[i])
+            {
+                answerText.text = "WRONG";
+                correct = false;
+            }
         }
+
+            
+            //checkSentence += textCheck.text + " ";
+        }
+
+            
+        answerImage.SetActive(true);
+        nextButton.gameObject.SetActive(true);
+        return correct;
     }
 
+    public void SpawnNewSentence()
+    {
+        answerImage.SetActive(false);
+        nextButton.gameObject.SetActive(false);
+
+        for(int i = 0; i < grid.transform.childCount; i++)
+        {
+            Transform space = grid.transform.GetChild(i);
+            
+            foreach(Transform child in space.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        GenerateCutUpSentence();
+    }
 }
