52ba0a00e75caed07dac14c6790eee39f1c4a259
cassandras12 <cassandra.shearwood@student.uts.edu.au>
2025-10-23 03:35:59 +1100

    add home button and quit button to lesson scene

--- WordWizGame/Assets/Cass/Categories/LessonButtons.cs
+++ WordWizGame/Assets/Cass/Categories/LessonButtons.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LessonButtons : MonoBehaviour
 {
@@ -12,5 +13,15 @@ public class LessonButtons : MonoBehaviour
     public void CloseWords()
     {
         wordsLesson.SetActive(false);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
